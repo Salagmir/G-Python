fe828082fad6a9cf5d8d75b87cb7078bef0fde16
Владислав Чурсин <np-slk@outlook.com>
2023-04-07 13:49:57 +0300

    Дз к 6 уроку

--- Проект/Урок 6. Практическое задание/task_2.py
+++ Проект/Урок 6. Практическое задание/task_2.py
@@ -16,3 +16,24 @@ length (длина в метрах), width (ширина в метрах).
 
 Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
 """
+
+
+class Road:
+    asphalt_mass = 25
+    thickness_of_the_canvas = 0.05
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def calculation_of_asphalt_mass(self):
+        try:
+            a = int(self._length * self._width * self.asphalt_mass * self.thickness_of_the_canvas)
+        except TypeError:
+            return None
+        else:
+            return f"{a} кг = {a // 1000} т"
+
+
+x = Road(20, 5000)
+print(x.calculation_of_asphalt_mass())
--- Проект/Урок 6. Практическое задание/task_3.py
+++ Проект/Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,33 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def __str__(self):
+        return f"{self.name} {self.surname} {self.position}"
+
+    def get_full_name(self):
+        return [self.name, self.surname]
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+
+my_name = "Иван"
+my_surname = "Иванович"
+my_position = "Программист"
+my_wage = 200
+my_bonus = 400
+
+x = Position(my_name, my_surname, my_position, my_wage, my_bonus)
+print(x.get_full_name(), x.get_total_income())
+print(x)
